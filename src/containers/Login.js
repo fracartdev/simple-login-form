46ec775f813afc411ed46745430a8154546c1e93
Francesco Cartier <francesco.cartier@testudosrl.com>
2021-02-10 11:49:23 +0100

    aggiunti useState al form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,27 +1,51 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import "./Login.css";
+import React, { useState } from "react";
 
 export default function Login() {
-    return (
-        <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(email, password)
+    }
+
+    return (
+        <div className="Login">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                        type="email" 
+                        placeholder="Enter email" 
+                        autoFocus 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    Login
+                </Button>
+            </Form>
+        </div>
     );
 }
